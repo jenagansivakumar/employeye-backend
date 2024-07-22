c22a77a2f67e8cb9d8f52a3b0f164f90adde06df
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-07-22 16:47:26 +0200

    finished Profile interface and schema

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -1,15 +1,31 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from 'mongoose';
 
 interface IProfile extends Document {
-    userId: String,
-    name: String,
-    email: String,
-    phone: Number,
-    address: String,
-    experience: String,
-    education: String,
-    skills: String,
-    createdAt: Date,
-    updatedAt: Date
+  userId: mongoose.Schema.Types.ObjectId;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  experience: string;
+  education: string;
+  skills: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
+const ProfileSchema: Schema = new Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  experience: { type: String, required: true },
+  education: { type: String, required: true },
+  skills: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Profile = mongoose.model<IProfile>('Profile', ProfileSchema);
+
+export default Profile;
