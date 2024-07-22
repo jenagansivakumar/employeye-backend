a78970c09ab12d735409e11a50351a743a753496
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-07-22 16:53:21 +0200

    completed users interface and schema

---
+++ src/models/Users.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model<IUser>('User', UserSchema);
+
+export default User;
